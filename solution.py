4bbe6ef5a03803ed4e562f9d7152d92cbf31c1ac
DanOs2300 <87863552+DanOs2300@users.noreply.github.com>
2023-04-13 22:35:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,15 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    from scipy.stats import ks_2samp
+    if len(x) == 0 or len(y) == 0:
+        return False
+    
+    stat, p_value = ks_2samp(x, y)
+    
+    alpha = 0.05
+    
+    if p_value < alpha:
+        return False
+    
+    return True
