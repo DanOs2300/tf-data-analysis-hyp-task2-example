8a098891bf39480ebf417f6620e97c30f2072260
DanOs2300 <87863552+DanOs2300@users.noreply.github.com>
2023-04-21 23:04:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,11 @@ import numpy as np
 chat_id = 630197911 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
+    from hyppo.ksample import MMD
     
-    res = cramervonmises_2samp(x, y)
-    
-    return res.pvalue < 0.03
+    res = MMD(compute_kernel='rbf', gamma=1.0).test(x, y)
+    if res[1] < 0.03:
+        bool_final = True
+    else:
+        bool_final = False
+    return bool_final
