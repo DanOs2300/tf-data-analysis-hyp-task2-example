edfea72ee47379d4564beee93a28d354c83a0dc7
DanOs2300 <87863552+DanOs2300@users.noreply.github.com>
2023-04-21 22:59:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,15 +5,7 @@ import numpy as np
 chat_id = 630197911 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    from scipy.stats import ks_2samp
-    if len(x) == 0 or len(y) == 0:
-        return False
     
-    stat, p_value = ks_2samp(x, y)
+    res = cramervonmises_2samp(x, y)
     
-    alpha = 0.05
-    
-    if p_value < alpha:
-        return False
-    
-    return True
+    return res.pvalue < 0.03
